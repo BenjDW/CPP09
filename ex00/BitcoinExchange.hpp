1d7e1c4ab1a588b271fd378e333498a385801546
BenjDW <dewitskibenji@live.fr>
2025-03-23 10:38:29 -0700

    start ex00

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 10:36:13 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/03/23 10:37:36 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <ostream>
+#include <iomanip>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <map>
+
