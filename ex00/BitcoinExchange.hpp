e69d0459fabc3d427827dc70a9481b6211e06b17
BenjDW <dewitskibenji@live.fr>
2025-03-23 11:15:05 -0700

    comments subject consigne , cannonical form struct

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 10:36:13 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/23 10:37:36 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/23 11:13:20 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,30 @@
 #include <list>
 #include <map>
 
+// Votre programme doit respecter ces règles:
+// • Le nom du programme est btc.
+// • Votre programme doit prendre un fichier en argument.
+// • Chaque ligne de ce fichier doit utiliser le format suivant: "date | valeur".
+// • Une date valide sera toujours au format "Année-Mois-Jour" (YYYY-MM-DD).
+// • Une valeur valide doit être soit un nombre à virgule flottante (float), ou un entier
+// positif, compris entre 0 et 1000.
+
+class BitcoinExchange
+{
+private:
+	
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &cpy);
+	BitcoinExchange	operator=(const BitcoinExchange &cpy);
+	~BitcoinExchange();
+	
+};
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,32 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 10:36:19 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/23 10:36:33 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/23 11:13:25 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+// Vous devez créer un programme qui affiche la valeur d’une certaine quantité de bitcoins
+// à une certaine date.
+// Ce programme doit utiliser une base de données au format CSV qui représentera le
+// prix du bitcoin au fil du temps. Cette base de données est fournie avec le sujet.
+
+// Le programme prendra en entrée une deuxième base de données stockant les différents
+// prix / dates à évaluer.
+
+// Votre programme doit respecter ces règles:
+// • Le nom du programme est btc.
+// • Votre programme doit prendre un fichier en argument.
+// • Chaque ligne de ce fichier doit utiliser le format suivant: "date | valeur".
+// • Une date valide sera toujours au format "Année-Mois-Jour" (YYYY-MM-DD).
+// • Une valeur valide doit être soit un nombre à virgule flottante (float), ou un entier
+// positif, compris entre 0 et 1000.
+
+int	main(int argc, char **argv)
+{
+	
+	if (argc != 3)// prend 2 base de données en entrées
+		std::cerr << "Error arg attend : base_csv base_de_donnée" << std::endl;
+	
+}
